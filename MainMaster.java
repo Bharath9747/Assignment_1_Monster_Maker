6057b42469d685402a49a87c0cb089401197b9eb
Bharath9747 <152379838+Bharath9747@users.noreply.github.com>
2023-12-10 21:00:22 +0530

    Changed Using Reflection

--- MainMaster.java
+++ MainMaster.java
@@ -11,23 +11,41 @@ public class MainMaster {
         Scanner sc = new Scanner(System.in);
         Set<Monster> monsterSet = new HashSet<>();
         Set<Monster> babyMonsterSet  = new HashSet<>();
+        System.out.println("Existing Attributes : ");
+        Monster monster1 = new Monster();
+        monster1.displayAtrribute();
+
+
         System.out.println("Enter the n Monsters : ");
 
         int n = sc.nextInt();
         for(int i=0;i<n;i++)
         {
             System.out.println("Monster : "+(i+1));
+            //Color of Eye
             String s1 = sc.next();
+            //Feather Color
             String s2 = sc.next();
-
+            //Magical Ability
             boolean s3 = sc.nextBoolean();
+            //Size
             int s4 = sc.nextInt();
+            //Strength
             int s5 = sc.nextInt();
+            //Durability
             int s6 = sc.nextInt();
+            //Aggressionlevel
             int s7 = sc.nextInt();
 
             Monster monster = new Monster(s1,s2,s3,s4,s5,s6,s7);
-
+            System.out.println("Do you want Attribute(Y/N)");
+            String ch = sc.next();
+            if(ch.equals("Y"))
+            {
+                String attributeName = sc.next();
+                String attributeValue = sc.next();
+                monster.setAdditionalAttribute(attributeName,attributeValue);
+            }
             if(monsterSet.add(monster)) {
                 System.out.println("Monster : " + (i + 1) + " Created");
                 if(monsterSet.size()==1)
@@ -49,9 +67,11 @@ public class MainMaster {
                     }
                     System.out.println("Monster List After Monster "+(i+1)+" Added");
                     monsterSet.stream().forEach(System.out::println);
+                    if(babyMonsterSet.size()!=0)
+                    {
                     System.out.println("Baby Monster List After Monster "+(i+1)+" Added");
                     babyMonsterSet.stream().forEach(System.out::println);
-                }
+                }}
 
             }
             else
--- Monster.java
+++ Monster.java
@@ -1,5 +1,6 @@
 package Monster_Maker;
 
+import java.lang.reflect.Field;
 import java.util.*;
 
 public class Monster {
@@ -10,29 +11,43 @@ public class Monster {
     int strength;
     int durability;
     int agressionLevel;
+    HashMap<String,Object> additionalAttribute = new HashMap<>();
+
+    public Object getAdditionalAttribute(String attributeName) {
+        return additionalAttribute.get(attributeName);
+    }
+
+    public void setAdditionalAttribute(String attributeName,Object attributeValue) {
+        additionalAttribute.put(attributeName,attributeValue);
+    }
+
+    public Monster() {
+
+    }
 
     @Override
     public String toString() {
-        return
-                "colorOfEye='" + colorOfEye + '\'' +
+        return  "colorOfEye='" + colorOfEye + '\'' +
                 ", featherColor='" + featherColor + '\'' +
                 ", magicalability=" + magicalability +
                 ", size=" + size +
                 ", strength=" + strength +
                 ", durability=" + durability +
-                ", agressionLevel=" + agressionLevel ;
+                ", agressionLevel=" + agressionLevel +
+                ", additionalAttribute=" + additionalAttribute;
     }
+
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof Monster monster)) return false;
-        return magicalability == monster.magicalability && size == monster.size && strength == monster.strength && durability == monster.durability && agressionLevel == monster.agressionLevel && Objects.equals(colorOfEye, monster.colorOfEye) && Objects.equals(featherColor, monster.featherColor);
+        return magicalability == monster.magicalability && size == monster.size && strength == monster.strength && durability == monster.durability && agressionLevel == monster.agressionLevel && Objects.equals(colorOfEye, monster.colorOfEye) && Objects.equals(featherColor, monster.featherColor) && Objects.equals(additionalAttribute, monster.additionalAttribute);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(colorOfEye, featherColor, magicalability, size, strength, durability, agressionLevel);
+        return Objects.hash(colorOfEye, featherColor, magicalability, size, strength, durability, agressionLevel,additionalAttribute);
     }
 
     public Monster(String colorOfEye, String featherColor, boolean magicalability, int size, int strength, int durability, int agressionLevel) {
@@ -61,10 +76,29 @@ public class Monster {
                 int strength = random.nextBoolean()?m1.strength:m2.strength;
                 int durability = random.nextBoolean()?m1.durability:m2.durability;
                 int agressionLevel = random.nextBoolean()?m1.agressionLevel:m2.agressionLevel;
-                Monster babyMonster = new Monster(colorOfEye,featherColor,magicalability,size,strength,durability,agressionLevel);
+                Monster babyMonster = new Monster(colorOfEye, featherColor, magicalability, size, strength, durability, agressionLevel);
                 babyMonsterSet.add(babyMonster);
             }
         }
         return babyMonsterSet;
+    }
+
+    /*public void addAttribute(String attributeName,Object attributValue){
+        try{
+            Field field = getClass().getDeclaredField(attributeName);
+            field.setAccessible(true);
+            field.set(this,attributValue);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }*/
+    public void displayAtrribute()  {
+        Field[] fields = getClass().getDeclaredFields();
+        for (Field field:fields)
+        {
+            field.setAccessible(true);
+            System.out.println(field.getName());
+        }
     }
 }
